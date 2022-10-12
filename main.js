d810ab4f0a4cc277cb369b68765e39e8958afc9c
hamza <hmas002@protonmail.com>
2022-10-12 15:30:16 -0400

    Add temporary ui so user does not have to play through console

--- main.js
+++ main.js
@@ -16,6 +16,12 @@ output winner.
 
 */
 
+const buttons = document.querySelectorAll('button');
+const resultsContainer = document.querySelector('.results');
+const choices = document.createElement('p');
+const results = document.createElement('p');
+
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -25,9 +31,9 @@ function getComputerChoice() {
     let choice = getRandomInt(1, 3);
     //console.log(choice);
 
-    if (choice === 1) return "Rock";
-    else if (choice === 2) return "Paper";
-    else if (choice === 3) return "Scissor";
+    if (choice === 1) return "rock";
+    else if (choice === 2) return "paper";
+    else if (choice === 3) return "scissor";
     else return "ERROR ON COMPUTER CHOICE";
 }
 
@@ -37,19 +43,19 @@ function playRound(computerChoice, playerChoice) {
     const player = playerChoice.toLowerCase();
 
     if (computer === player) {
-        console.log(`This is a tie. You both picked ${computerChoice}. Try again or dont i do not really care.`);
+        results.textContent = `This is a tie. You both picked ${computerChoice}. Try again or dont i do not really care.`;
         return 0;
     }
     else if ((computer === 'rock' && player === "scissor") || (computer === 'scissor' && player === "paper") || (computer === 'paper' && player === "rock")) {
-        console.log(`You is a loser! ${computerChoice} beats ${playerChoice}`);
+        results.textContent = `You is a loser! ${computerChoice} beats ${playerChoice}`;
         return -1;
     }
     else {
-        console.log(`You is a winner! ${playerChoice} beats ${computerChoice}`);
+        results.textContent = `You is a winner! ${playerChoice} beats ${computerChoice}`;
         return 1;
     }
 }
-
+console.log(results);
 /* quick test 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
@@ -85,10 +91,21 @@ function game() {
         if (winner > 0) playerRounds++;
         else if (winner < 0) computerRounds++;
     }
-    
+
     console.log(`Computer: ${computerRounds} Human: ${playerRounds}`);
     if (playerRounds > computerRounds) console.log("You win! Good Job!!!!")
     else if (computerRounds > playerRounds) console.log('You is a loser lolololololol');
-    else console.log("you tied which is pretyr much a loss lololololo!!!");
+    else console.log("you tied which is pretty much a loss lololololo!!!");
 }
-game();
+//game();
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    //console.log(button.className);
+    let compChoice = getComputerChoice();
+    choices.textContent = `Computer picked ${compChoice}. Human picked ${button.className}.`;
+
+    playRound(compChoice, button.className);
+}));
+
+resultsContainer.appendChild(choices);
+resultsContainer.appendChild(results);
